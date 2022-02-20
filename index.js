7723452bb6aebc92b47e9f900aff5794dca07736
Marcus Crane <marcus@utf9k.net>
2022-02-20 20:40:04 +1300

    Fix signer function naming

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const AWS = require('aws-sdk');
 
 const core = require('@actions/core')
-const github = require('@actions/github')
 
 try {
   const signer = new AWS.CloudFront.Signer(
     core.getInput('cfDistId'),
     core.getInput('privateKey')
   )
-  const signedUrl = signer.getSignedURL({
+  const signedUrl = signer.getSignedUrl({
     url: core.getInput('url'),
     expires: core.getInput('expires')
   })
