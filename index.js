9e89a4280366668170a02db6d5b0183bff706f69
Marcus Crane <marcus@utf9k.net>
2022-02-21 13:02:48 +1300

    Try to fix up private key

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const core = require('@actions/core')
 try {
   const signer = new AWS.CloudFront.Signer(
     core.getInput('cfDistId'),
-    core.getInput('privateKey')
+    core.getInput('privateKey').replace(/\\n/gm, '\n') // https://github.com/auth0/node-jsonwebtoken/issues/642#issuecomment-601720769
   )
   const signedUrl = signer.getSignedUrl({
     url: core.getInput('url'),
